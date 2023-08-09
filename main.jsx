4ba77c292384db9dd4d8ba8c0d2033ce25389f95
Karkala Vikas reddy <71484696+kkkiqjn@users.noreply.github.com>
2023-08-09 13:27:24 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'C:\\Users\\saket\\meghavi_form_page\\src\\form.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
